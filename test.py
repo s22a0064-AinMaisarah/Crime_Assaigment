d8336ec771f956a8f8c0ba3ace54be784705790e
s22a0064-AinMaisarah <s22a0064@siswa.umk.edu.my>
2025-11-02 02:42:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
 import streamlit as st
-st.set_page_config(
-    page_title="SCrime Analytics Dashboard"
+
+st.set_page_config(page_title="Crime Analytics Dashboard", page_icon="🚓")
+
+home = st.Page("home.py", title="🏠 Home", default=True)
+page1 = st.Page("Page1_Clustering.py", title="📊 Crime Clustering (PCA)")
+page2 = st.Page("Page2_IncomeCrime.py", title="💰 Income vs Crime Analysis")
+page3 = st.Page("Page3_RadarAge.py", title="🧭 Crime Radar by Age Group")
+
+navigation = st.navigation(
+    {
+        "Menu": [home, page1, page2, page3]
+    }
 )
-visualise = st.Page('Crime.py', title='Crime Visualization Dashboard ', icon=":material/school:")
 
-home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
-
-pg = st.navigation(
-        {
-            "Menu": [home, visualise]
-        }
-    )
-
-pg.run()
+navigation.run()
